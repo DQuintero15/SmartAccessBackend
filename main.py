a65eafccd3e819d49518600b6db32afdc269be26
DQuintero15 <quinterod164@gmail.com>
2024-04-01 17:29:51 -0500

    add profile module for general routes

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 
 from fastapi import FastAPI
+from src.routes.profile import profile_router
 from src.routes.auth import auth_router
 
 app = FastAPI(title="SmartAccess API", version="0.1.0", summary="API for SmartAccess")
 
 app.include_router(auth_router, prefix="/auth", tags=["Authentication"])
+app.include_router(profile_router, prefix="/profile", tags=["Profile"])
 
 @app.get("/", tags=["root"])
 def read_root():
