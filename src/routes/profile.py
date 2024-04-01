a2479c7e942db8a663f447dfe2241ec3923cd3ff
DQuintero15 <quinterod164@gmail.com>
2024-04-01 17:37:24 -0500

    feat: :label: add user profile schema response

--- src/routes/profile.py
+++ src/routes/profile.py
@@ -2,9 +2,10 @@ from fastapi import APIRouter, Depends
 from src.config.supabase import supa
 from src.security.JwtBearer import JWTBearer
 
+from src.schemas.profile import UserProfileSchema
 
 profile_router = APIRouter()
 
 @profile_router.get("", tags=["Profile"], dependencies = [Depends(JWTBearer())])
-async def get_profile():
-    return supa.auth.get_user()
+async def get_profile() -> UserProfileSchema:
+    return supa.auth.get_user().user.user_metadata
