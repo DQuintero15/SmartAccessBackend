a822748fac60af00375482bc239b25f555cf4200
DQuintero15 <quinterod164@gmail.com>
2024-04-22 18:02:52 -0500

    feat: add hook handler

--- src/routes/auth.py
+++ src/routes/auth.py
@@ -47,3 +47,32 @@ async def signin(sign_in_data: SignInSchema):
         if e.__class__.__name__ == "AuthApiError":
             return JSONResponse(status_code=401, content=jsonable_encoder({"detail": str(e)}))
         return JSONResponse(status_code=500, content=jsonable_encoder({"detail": str(e)}))
+
+@auth_router.post("/refresh", tags=["Authentication"])
+async def refresh_token():
+    try:
+        new_session = supa.auth.refresh_session()
+        return JSONResponse(status_code=200, content=jsonable_encoder(new_session))
+    
+    except RequestValidationError as e:
+        return JSONResponse(status_code=400, content=jsonable_encoder({"detail": e.errors()}))
+    
+    except Exception as e:
+        if e.__class__.__name__ == "AuthApiError":
+            return JSONResponse(status_code=401, content=jsonable_encoder({"detail": str(e)}))
+        return JSONResponse(status_code=500, content=jsonable_encoder({"detail": str(e)}))
+    
+@auth_router.post("/retrive-new-session", tags=["Authentication"])
+async def retrive_new_session():
+    try:
+        new_session = supa.auth.set_session("eyJhbGciOiJIUzI1NiIsImtpZCI6Ijd4Mk9COWxPeE1MalBIWGgiLCJ0eXAiOiJKV1QifQ.eyJhdWQiOiJhdXRoZW50aWNhdGVkIiwiZXhwIjoxNzEyMTQ1MzUxLCJpYXQiOjE3MTIxNDE3NTEsImlzcyI6Imh0dHBzOi8vZWFha2ZpZHBha3RqZnhhcXpmeWIuc3VwYWJhc2UuY28vYXV0aC92MSIsInN1YiI6IjUyMGJiNzI2LTkxYjEtNDZjYy1iNjBhLTMwYTg4ZDJiM2U4NCIsImVtYWlsIjoicXVpbnRlcm9kMTY0QGdtYWlsLmNvbSIsInBob25lIjoiIiwiYXBwX21ldGFkYXRhIjp7InByb3ZpZGVyIjoiZW1haWwiLCJwcm92aWRlcnMiOlsiZW1haWwiXX0sInVzZXJfbWV0YWRhdGEiOnsiYnVzaW5lc3NfbmFtZSI6IkF1dG8gb2FzaXMiLCJlbWFpbCI6InF1aW50ZXJvZDE2NEBnbWFpbC5jb20iLCJlbWFpbF92ZXJpZmllZCI6ZmFsc2UsImZpcnN0X25hbWUiOiJKb2huIiwibGFzdF9uYW1lIjoiRG9lIiwicGFya2luZ19mZWVfcGVyX21pbnV0ZSI6OTcsInBob25lX3ZlcmlmaWVkIjpmYWxzZSwicm9sZSI6ImFkbWluIiwic3ViIjoiNTIwYmI3MjYtOTFiMS00NmNjLWI2MGEtMzBhODhkMmIzZTg0In0sInJvbGUiOiJhdXRoZW50aWNhdGVkIiwiYWFsIjoiYWFsMSIsImFtciI6W3sibWV0aG9kIjoicGFzc3dvcmQiLCJ0aW1lc3RhbXAiOjE3MTIxNDE3NTF9XSwic2Vzc2lvbl9pZCI6IjFhZmVjOGNiLTRmMDUtNGZlMC1iZGUzLTM1MGQ1N2IxYjNhZSIsImlzX2Fub255bW91cyI6ZmFsc2V9.F5TbQco9KKPnfiuwie_wmjXATH3QKk2aqnfwpIEKyXo",
+                                            "LtNfHO5x3nsb7SJ0QilSmQ")
+        return JSONResponse(status_code=200, content=jsonable_encoder(new_session))
+    
+    except RequestValidationError as e:
+        return JSONResponse(status_code=400, content=jsonable_encoder({"detail": e.errors()}))
+    
+    except Exception as e:
+        if e.__class__.__name__ == "AuthApiError":
+            return JSONResponse(status_code=401, content=jsonable_encoder({"detail": str(e)}))
+        return JSONResponse(status_code=500, content=jsonable_encoder({"detail": str(e)}))
